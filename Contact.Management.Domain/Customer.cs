cf86977369c8768bfee8ac510b8571b602914d34
musavand <mehdimosavand@gmail.com>
2022-05-17 22:28:02 +0430

    appling sanitize data

--- Contact.Management.Domain/Customer.cs
+++ Contact.Management.Domain/Customer.cs
@@ -9,20 +9,24 @@ namespace Contact.Management.Domain
 {
     public class Customer : BaseDomainEntity
     {
+        private string _Firstname;
+        private string _Lastname;
+        private string _Email;
+        private string _BankAccountNumber;
 
-        public String? Firstname { get; set; }
-        public String? Lastname { get; set; }
+        public String? Firstname { get { return _Firstname.ToUpper(); } set { _Firstname = value; } }
+        public String? Lastname { get { return _Lastname.ToUpper(); } set { _Lastname = value; } }
         public DateTime DateOfBirth { get; set; }
         public String? PhoneNumber { get; set; }
-        public String? Email { get; set; }
+        public String? Email { get { return _Email.ToUpper(); } set { _Email = value; } }
 
-        public String? BankAccountNumber { get; set; }
-
-
+        public String? BankAccountNumber { get { return _BankAccountNumber.ToUpper(); } set { _BankAccountNumber = value; } }
 
 
 
 
-        
+
+
+
     }
 }
